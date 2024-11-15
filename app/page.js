f8c6e706498ce9d4fccc2988c98477d0fe7a38b2
MelloBirkan <mgbirkan@icloud.com>
2024-11-14 23:25:39 -0300

    Correćão da altura

--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ function HeroDetails({hero}) {
     <li key={hero.id} className={"flex flex-col"}>
       <img src={hero.image.url}
            className="w-full h-[600px] rounded-2xl object-cover"/>
-      <div className={"flex flex-col h-56"}>
+      <div className={"flex flex-col"}>
         <h3
           className="mt-6 text-lg/8 font-semibold text-gray-900">{hero.name}</h3>
         <p className="text-base/7 text-gray-600">{hero.work.occupation}</p>
@@ -24,7 +24,7 @@ function HeroDetails({hero}) {
           className="mt-4 text-base/7 text-gray-600">{`${heroBiography["full-name"]}, born in ${heroBiography["place-of-birth"]}, is a ${heroAppearance.gender.toLowerCase()} hero with a height of ${heroAppearance.height[1]} and a weight of ${heroAppearance.weight[1]}. They have ${heroAppearance["eye-color"].toLowerCase()} eyes and ${heroAppearance["hair-color"].toLowerCase()} hair. Their alignment is ${heroBiography.alignment}.`}</p>
       </div>
 
-      <ul>
+      <ul className={"mt-6"}>
         {Object.entries(heroPowerstats).map(([stat, value], index) => (
           <li key={stat} className={"flex gap-4 items-center"}>
             <p className={"w-28"}>{stat}</p>
