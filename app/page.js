5019d9011b1c75366a4b888a0fa1772cb432e7db
MelloBirkan <mgbirkan@icloud.com>
2024-11-18 12:37:27 -0300

    Agora todos os herois podem ser requisitados

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import ProgressBar from "@/app/clientComponents/ProgressBar";
 
-async function fetchHero(id = Math.floor(Math.random() * (730 - 1) + 1)) {
+async function fetchHero(id = Math.floor(Math.random() * (731) + 1)) {
   const apiToken = "1783ce1d5a619ad9a70829c9f95c3c0d";
   const apiUrl = `https://superheroapi.com/api/${apiToken}/${id}`;
 
